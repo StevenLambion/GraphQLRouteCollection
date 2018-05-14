04de0b68fa0f0bea97d42c494faabd17a7d9e2c0
Steven Lambion <stevenlambion@gmail.com>
2018-05-13 20:55:02 -0700

    Added GraphQLService as a instance variable to GraphQLRouteCollection

--- Sources/VaporGraphQL/GraphQLRouteCollection.swift
+++ Sources/VaporGraphQL/GraphQLRouteCollection.swift
@@ -6,19 +6,22 @@ import GraphQL
 public struct GraphQLRouteCollection: RouteCollection {
   /// Path to the graphql route
   public let path: String
-  
+
+  public let graphql: GraphQLService
+
   /// Enables the GraphiQL webpage
   public let enableGraphiQL: Bool
-  
+
   /// Path to GraphiQL. Defaults to "/graphiql"
   public let graphiQLPath: String
-  
-  public init(_ path: String = "graphql", enableGraphiQL: Bool = false, graphiQLPath: String = "graphiql") {
+
+  public init(_ path: String = "graphql", using graphql: GraphQLService, enableGraphiQL: Bool = false, graphiQLPath: String = "graphiql") {
     self.path = path
+    self.graphql = graphql
     self.enableGraphiQL = enableGraphiQL
     self.graphiQLPath = graphiQLPath
   }
-  
+
   public func boot(router: Router) throws {
     let graphql = router.grouped(path)
     graphql.get("", use: self.execute)
@@ -27,14 +30,12 @@ public struct GraphQLRouteCollection: RouteCollection {
       router.get(graphiQLPath, use: self.renderGraphiQL)
     }
   }
-  
+
   private func execute(_ req: Request) throws -> Future<Map> {
-    let graphql = try req.make(GraphQLService.self)
     return try graphql.execute(req)
   }
-  
+
   private func renderGraphiQL(_ req: Request) throws -> View {
-    let graphql = try req.make(GraphQLService.self)
     return try graphql.renderGraphiQL(pathToGraphQL: path, for: req)
   }
 }
